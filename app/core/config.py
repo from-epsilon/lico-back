8f9447f7814e9ecd5e74fb81ba8ce25af9a6e2ec
jewan100 <hawaii1468@gmail.com>
2026-01-06 17:25:38 +0900

    refactor: Spring으로 프로젝트 변경

--- app/core/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-  PROJECT_NAME: str = "MyApp API"
-  class Config:
-    env_file = ".env"
-
-settings = Settings()
---
+++ src/main/kotlin/com/epsilon/nagginggnome/NagginggnomeApplication.kt
@@ -0,0 +1,11 @@
+package com.epsilon.nagginggnome
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NagginggnomeApplication
+
+fun main(args: Array<String>) {
+	runApplication<NagginggnomeApplication>(*args)
+}
