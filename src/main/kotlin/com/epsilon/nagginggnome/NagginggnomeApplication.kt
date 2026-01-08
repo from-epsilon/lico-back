09131f78f229c543fbeda24271309acb816513bc
jewan100 <hawaii1468@gmail.com>
2026-01-08 16:40:23 +0900

    feat: `ConfigurationProperties` 스캔하도록 어노테이션 추가

--- src/main/kotlin/com/epsilon/nagginggnome/NagginggnomeApplication.kt
+++ src/main/kotlin/com/epsilon/nagginggnome/NagginggnomeApplication.kt
@@ -1,9 +1,11 @@
 package com.epsilon.nagginggnome
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class NagginggnomeApplication
 
 fun main(args: Array<String>) {
