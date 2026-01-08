338ae2d46c313db93190a7842935a7af3d7398a5
jewan100 <hawaii1468@gmail.com>
2026-01-08 18:43:22 +0900

    feat: 엔드포인트 /api/v*으로 고정

---
+++ src/main/kotlin/com/epsilon/nagginggnome/global/config/ApiVersionPrefixConfig.kt
@@ -0,0 +1,21 @@
+package com.epsilon.nagginggnome.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.method.HandlerTypePredicate
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+/**
+ * 모든 @RestController 엔드포인트 경로 앞에 프리픽스를 자동으로 붙이는 설정 클래스입니다.
+ */
+@Configuration
+class ApiVersionPrefixConfig : WebMvcConfigurer {
+
+    override fun configurePathMatch(configurer: PathMatchConfigurer) {
+        configurer.addPathPrefix(
+            "/v1",
+            HandlerTypePredicate.forAnnotation(RestController::class.java)
+        )
+    }
+}
