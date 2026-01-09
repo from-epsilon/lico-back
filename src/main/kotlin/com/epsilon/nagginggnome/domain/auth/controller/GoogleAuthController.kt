e1e74b21d53c90c8e15b21f85b0e05becab03180
jewan100 <hawaii1468@gmail.com>
2026-01-09 17:00:49 +0900

    feat: google 소셜 로그인 컨트롤러 작성

---
+++ src/main/kotlin/com/epsilon/nagginggnome/domain/auth/controller/GoogleAuthController.kt
@@ -0,0 +1,30 @@
+package com.epsilon.nagginggnome.domain.auth.controller
+
+import com.epsilon.nagginggnome.domain.auth.dto.request.SocialLoginRequest
+import com.epsilon.nagginggnome.domain.auth.dto.response.SocialLoginResponse
+import com.epsilon.nagginggnome.domain.auth.service.GoogleAuthService
+import com.epsilon.nagginggnome.global.constant.code.CommonSuccessCode
+import com.epsilon.nagginggnome.global.dto.response.ApiResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/auth/social/google")
+class GoogleAuthController(
+    private val googleAuthService: GoogleAuthService
+) {
+
+    /**
+     * 소셜 로그인 API
+     */
+    @PostMapping("/login")
+    fun login(
+        @RequestBody req: SocialLoginRequest
+    ): ResponseEntity<ApiResponse<SocialLoginResponse>> {
+        val res = googleAuthService.loginOrSignUp(req)
+        return ResponseEntity.ok(ApiResponse.ok(CommonSuccessCode.SUCCESS, res))
+    }
+}
