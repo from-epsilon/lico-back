2348fbff6fff5e77f7ec874b71da9305b028ffc0
jewan100 <hawaii1468@gmail.com>
2026-01-09 13:36:33 +0900

    refactor: code 관련 패키지 변경

--- src/main/kotlin/com/epsilon/nagginggnome/global/constant/code/CommonErrorCode.kt
+++ src/main/kotlin/com/epsilon/nagginggnome/global/constant/code/CommonErrorCode.kt
@@ -1,4 +1,4 @@
-package com.epsilon.nagginggnome.global.code
+package com.epsilon.nagginggnome.global.constant.code
 
 import org.springframework.http.HttpStatus
 
@@ -9,22 +9,27 @@ enum class CommonErrorCode(
 ) : ErrorCode {
 
     /**
-     * 요청 형식 오류
+     * 요청 값이 유효하지 않거나 요청 형식이 올바르지 않은 경우
      */
     INVALID_REQUEST("COMMON_400", HttpStatus.BAD_REQUEST, "Bad Request"),
 
     /**
-     * 인증 오류
+     * 인증에 실패한 경우
      */
     UNAUTHORIZED("COMMON_401", HttpStatus.UNAUTHORIZED, "Unauthorized"),
 
     /**
-     * 권한 오류
+     * 인증은 되었으나 접근 권한이 없는 경우
      */
     FORBIDDEN("COMMON_403", HttpStatus.FORBIDDEN, "Forbidden"),
 
     /**
-     * 서버 내부 오류
+     * 요청한 리소스를 찾을 수 없는 경우
+     */
+    NOT_FOUND("COMMON_404", HttpStatus.NOT_FOUND, "Not Found"),
+
+    /**
+     * 서버 내부 처리 중 예기치 못한 오류가 발생한 경우
      */
     INTERNAL_ERROR("COMMON_500", HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error")
 }
