8b439c920aa13df5913a1b7be4c94c5075c01c43
jewan100 <hawaii1468@gmail.com>
2026-01-14 18:03:19 +0900

    chore: 누락된 주석 추가

--- src/main/kotlin/com/epsilon/nagginggnome/global/security/crypto/TokenHasher.kt
+++ src/main/kotlin/com/epsilon/nagginggnome/global/security/crypto/TokenHasher.kt
@@ -3,10 +3,13 @@ package com.epsilon.nagginggnome.global.security.crypto
 import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
 import org.springframework.stereotype.Component
-import java.util.Base64
+import java.util.*
 import javax.crypto.Mac
 import javax.crypto.SecretKey
 
+/**
+ * 토큰 해싱 컴포넌트
+ */
 @Component
 class TokenHasher(
     private val props: TokenHashProperties
@@ -14,6 +17,9 @@ class TokenHasher(
 
     private val key: SecretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(props.pepper))
 
+    /**
+     * 입력 토큰을 HMAC-SHA256으로 해싱하여 Base64 문자열로 반환합니다.
+     */
     fun hash(token: String): String {
         val mac = Mac.getInstance(CryptoConstants.HMAC_SHA256)
         mac.init(key)
