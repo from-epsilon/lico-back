d9b23a18d5b95aeaaaa249ac2feac481e9c6adef
jewan100 <hawaii1468@gmail.com>
2026-01-17 23:34:34 +0900

    feat: Plan 생성 요청, 응답 DTO 작성

---
+++ src/main/kotlin/com/epsilon/nagginggnome/domain/plan/dto/request/PlanCreateRequest.kt
@@ -0,0 +1,15 @@
+package com.epsilon.nagginggnome.domain.plan.dto.request
+
+import java.time.Instant
+
+/**
+ * 플랜 생성 요청 DTO
+ */
+data class PlanCreateRequest(
+    val action: String,
+    val rrule: String,
+    val dtstart: Instant,
+    val purpose: String?,
+    val motive: String?,
+    val memo: String?
+)
