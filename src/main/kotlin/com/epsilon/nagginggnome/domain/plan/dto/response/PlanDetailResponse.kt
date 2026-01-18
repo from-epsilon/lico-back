604215256f2b104caea41d8da1593620fc4ebbde
jewan100 <hawaii1468@gmail.com>
2026-01-18 19:22:02 +0900

    feat: 플랜 조회 관련 응답 DTO 생성 및 수정

--- src/main/kotlin/com/epsilon/nagginggnome/domain/plan/dto/response/PlanDetailResponse.kt
+++ src/main/kotlin/com/epsilon/nagginggnome/domain/plan/dto/response/PlanDetailResponse.kt
@@ -3,13 +3,16 @@ package com.epsilon.nagginggnome.domain.plan.dto.response
 import java.time.Instant
 
 /**
- * 플랜 리스트 조회 응답 DTO
+ * 플랜 상세 조회 응답 DTO
  */
-data class PlanListItemResponse(
+data class PlanDetailResponse(
     val planId: Long,
     val currentVersion: Int,
     val currentSnapshotId: Long,
     val action: String,
     val rrule: String,
     val dtstart: Instant,
+    val purpose: String?,
+    val motive: String?,
+    val memo: String?
 )
