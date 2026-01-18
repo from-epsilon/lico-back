76a60716c5d16a45c468d65b4333717d1a0ecc77
jewan100 <hawaii1468@gmail.com>
2026-01-18 18:18:04 +0900

    feat: Plan을 찾지 못했을 경우에 대한 에러코드 추가

--- src/main/kotlin/com/epsilon/nagginggnome/global/constant/code/PlanErrorCode.kt
+++ src/main/kotlin/com/epsilon/nagginggnome/global/constant/code/PlanErrorCode.kt
@@ -16,4 +16,13 @@ enum class PlanErrorCode(
         status = HttpStatus.INTERNAL_SERVER_ERROR,
         message = "Failed to create plan."
     ),
+
+    /**
+     * 플랜을 찾지 못한 경우
+     */
+    PLAN_NOT_FOUND(
+        code = "PLAN_404_NOT_FOUND",
+        status = HttpStatus.NOT_FOUND,
+        message = "Plan not found."
+    ),
 }
