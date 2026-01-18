1f25d931951f62edf5245d021e860f710b5a6a43
jewan100 <hawaii1468@gmail.com>
2026-01-18 23:44:35 +0900

    feat: Message 타입 추가

---
+++ src/main/kotlin/com/epsilon/nagginggnome/domain/message/constant/MessageType.kt
@@ -0,0 +1,22 @@
+package com.epsilon.nagginggnome.domain.message.constant
+
+/**
+ * 메시지 타입
+ */
+enum class MessageType {
+
+    /**
+     * 잔소리
+     */
+    NAGGING,
+
+    /**
+     * 플랜 히스토리
+     */
+    PLAN_HISTORY,
+
+    /**
+     * 유저 답장
+     */
+    USER_REPLY
+}
