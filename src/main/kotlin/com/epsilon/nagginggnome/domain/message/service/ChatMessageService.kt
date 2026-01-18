34434989303873087701cae55d89de18d0eab987
jewan100 <hawaii1468@gmail.com>
2026-01-19 00:09:44 +0900

    feat: ChatMessage 관련 Service 작성 및 plan 히스토리 추가 메서드 작성

---
+++ src/main/kotlin/com/epsilon/nagginggnome/domain/message/service/ChatMessageService.kt
@@ -0,0 +1,25 @@
+package com.epsilon.nagginggnome.domain.message.service
+
+import com.epsilon.nagginggnome.domain.message.constant.MessageType
+import com.epsilon.nagginggnome.domain.message.entity.ChatMessage
+import com.epsilon.nagginggnome.domain.message.repository.ChatMessageRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class ChatMessageService(
+    private val chatMessageRepository: ChatMessageRepository
+) {
+    @Transactional
+    fun appendPlanHistoryMessage(planId: Long, snapshotId: Long, version: Int, content: String) {
+        chatMessageRepository.save(
+            ChatMessage(
+                planId = planId,
+                snapshotId = snapshotId,
+                version = version,
+                content = content,
+                type = MessageType.PLAN_HISTORY
+            )
+        )
+    }
+}
