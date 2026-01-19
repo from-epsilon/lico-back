070238fedf73bcc316bf772b6ef8239293ae33c9
jewan100 <hawaii1468@gmail.com>
2026-01-20 00:49:51 +0900

    test: 불필요한 테스트 제거

--- src/test/kotlin/com/epsilon/nagginggnome/NagginggnomeApplicationTests.kt
+++ src/test/kotlin/com/epsilon/nagginggnome/NagginggnomeApplicationTests.kt
@@ -1,13 +1,8 @@
 package com.epsilon.nagginggnome
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class NagginggnomeApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
 
 }
