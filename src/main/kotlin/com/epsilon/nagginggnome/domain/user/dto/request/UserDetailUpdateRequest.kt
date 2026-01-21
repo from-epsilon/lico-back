f1eb6a032cace9f2b2773412f6e9ea2b7a84a048
jewan100 <hawaii1468@gmail.com>
2026-01-21 21:53:52 +0900

    feat: UserDetail 수정 요청 DTO 변경 및 응답 DTO 생성

--- src/main/kotlin/com/epsilon/nagginggnome/domain/user/dto/request/UserDetailUpdateRequest.kt
+++ src/main/kotlin/com/epsilon/nagginggnome/domain/user/dto/request/UserDetailUpdateRequest.kt
@@ -4,11 +4,8 @@ package com.epsilon.nagginggnome.domain.user.dto.request
  * 유저 상세 정보 수정 요청 DTO
  */
 data class UserDetailUpdateRequest(
-    val nickname: String,
-    val coreValue: String,
-    val motive: String,
-    val selfImage: String,
-    val verbosityPerDay: Double,
-    val sleepTime: Int,
-    val wakeTime: Int
+    val nickname: String?,
+    val verbosityPerDay: Double?,
+    val sleepTime: Int?,
+    val wakeTime: Int?
 )
