0a98db3f17a19afb03e09fd703b5e62f04b228f2
jewan100 <hawaii1468@gmail.com>
2026-01-21 15:23:55 +0900

    feat: FcmToken Repository 작성

---
+++ src/main/kotlin/com/epsilon/nagginggnome/domain/push/repository/FcmTokenRepository.kt
@@ -0,0 +1,10 @@
+package com.epsilon.nagginggnome.domain.push.repository
+
+import com.epsilon.nagginggnome.domain.push.entity.FcmToken
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface FcmTokenRepository : JpaRepository<FcmToken, UUID> {
+}
