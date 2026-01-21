d04b4638679e4345c8093378d477f26996ee109c
jewan100 <hawaii1468@gmail.com>
2026-01-21 19:17:09 +0900

    feat: UserDetail Repository 작성

---
+++ src/main/kotlin/com/epsilon/nagginggnome/domain/user/repository/UserDetailRepository.kt
@@ -0,0 +1,10 @@
+package com.epsilon.nagginggnome.domain.user.repository
+
+import com.epsilon.nagginggnome.domain.user.entity.UserDetail
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface UserDetailRepository : JpaRepository<UserDetail, UUID> {
+}
