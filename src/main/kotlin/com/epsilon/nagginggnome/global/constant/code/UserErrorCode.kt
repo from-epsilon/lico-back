851e2ffe545621362df7df073cf3f4ade655bec4
jewan100 <hawaii1468@gmail.com>
2026-01-21 20:03:22 +0900

    feat: UserDetail 관련 에러코드 추가

--- src/main/kotlin/com/epsilon/nagginggnome/global/constant/code/UserErrorCode.kt
+++ src/main/kotlin/com/epsilon/nagginggnome/global/constant/code/UserErrorCode.kt
@@ -19,4 +19,22 @@ enum class UserErrorCode(
         status = HttpStatus.NOT_FOUND,
         message = "User not found."
     ),
+
+    /**
+     * 유저의 상세 정보를 찾지 못한 경우
+     */
+    USER_DETAIL_NOT_FOUND(
+        code = "USER_DETAIL_404_NOT_FOUND",
+        status = HttpStatus.NOT_FOUND,
+        message = "User's detail not found."
+    ),
+
+    /**
+     * 유저의 상세 정보가 존재하는데 생성하려는 경우
+     */
+    USER_DETAIL_ALREADY_EXISTS(
+        code = "USER_DETAIL_409_CONFLICT",
+        status = HttpStatus.CONFLICT,
+        message = "User's detail already exists."
+    )
 }
