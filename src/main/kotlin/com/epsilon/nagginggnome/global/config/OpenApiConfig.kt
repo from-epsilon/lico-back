5b0be081105ad32b65b1c26ff1fa15568fe8569b
jewan100 <hawaii1468@gmail.com>
2026-01-22 00:59:33 +0900

    feat: JWT 권한 설정 추가

---
+++ src/main/kotlin/com/epsilon/nagginggnome/global/config/OpenApiConfig.kt
@@ -0,0 +1,32 @@
+package com.epsilon.nagginggnome.global.config
+
+import com.epsilon.nagginggnome.global.security.jwt.JwtConstants
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI {
+        val schemeName = JwtConstants.BEARER_PREFIX
+
+        return OpenAPI()
+            .addSecurityItem(SecurityRequirement().addList(schemeName))
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        schemeName,
+                        SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                    )
+            )
+    }
+}
