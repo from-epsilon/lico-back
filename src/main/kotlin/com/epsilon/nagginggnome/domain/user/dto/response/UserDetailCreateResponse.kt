59174efb7cc42862475a84059a30fef7956281c7
jewan100 <hawaii1468@gmail.com>
2026-01-21 22:32:02 +0900

    feat: null 응답값도 포함

--- src/main/kotlin/com/epsilon/nagginggnome/domain/user/dto/response/UserDetailCreateResponse.kt
+++ src/main/kotlin/com/epsilon/nagginggnome/domain/user/dto/response/UserDetailCreateResponse.kt
@@ -4,8 +4,8 @@ package com.epsilon.nagginggnome.domain.user.dto.response
  * 유저 상세 정보 생성 응답 DTO
  */
 data class UserDetailCreateResponse(
-    val nickname: String,
-    val verbosityPerDay: Double,
-    val sleepTime: Int,
-    val wakeTime: Int
+    val nickname: String?,
+    val verbosityPerDay: Double?,
+    val sleepTime: Int?,
+    val wakeTime: Int?
 )
