8ce8954ddfd5ac393371a17172a36c4c51516f1f
jewan100 <hawaii1468@gmail.com>
2026-01-21 21:49:45 +0900

    feat: user_details 테이블의 NotNull 제약 삭제

--- src/main/resources/db/migration/V4__create_user_details.sql
+++ src/main/resources/db/migration/V4__create_user_details.sql
@@ -1,12 +1,12 @@
 CREATE TABLE public.user_details (
     user_id uuid NOT NULL,
-    nickname character varying(255) NOT NULL,
-    core_value text NOT NULL,
-    motive text NOT NULL,
-    self_image text NOT NULL,
-    verbosity_per_day double precision NOT NULL,
-    sleep_time integer NOT NULL,
-    wake_time integer NOT NULL,
+    nickname character varying(255),
+    core_value text,
+    motive text,
+    self_image text,
+    verbosity_per_day double precision,
+    sleep_time integer,
+    wake_time integer,
 
     CONSTRAINT pk_user_details PRIMARY KEY (user_id),
     CONSTRAINT fk_user_details_user_id FOREIGN KEY (user_id) REFERENCES public.users(id)
