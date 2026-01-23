7c2a66010a4a6a1765f19ba2a6f39bbf2d106d58
jewan100 <hawaii1468@gmail.com>
2026-01-23 17:53:38 +0900

    feat: shedLock 분산락을 적용한 배치 설정 추가

---
+++ src/main/kotlin/com/epsilon/nagginggnome/global/config/BatchSchedulingConfig.kt
@@ -0,0 +1,35 @@
+package com.epsilon.nagginggnome.global.config
+
+import net.javacrumbs.shedlock.core.LockProvider
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.scheduling.annotation.EnableScheduling
+
+/**
+ * batch 프로파일에서만 스케줄러를 활성화하기 위한 설정 클래스
+ */
+@Profile("batch")
+@Configuration
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT10M")
+class BatchSchedulingConfig(
+    private val jdbcTemplate: JdbcTemplate
+) {
+
+    /**
+     * ShedLock 분산락 Provider 빈
+     */
+    @Bean
+    fun lockProvider(): LockProvider {
+        return JdbcTemplateLockProvider(
+            JdbcTemplateLockProvider.Configuration.builder()
+                .withJdbcTemplate(jdbcTemplate)
+                .usingDbTime()
+                .build()
+        )
+    }
+}
