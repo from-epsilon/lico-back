e56bfee8b603c22d217ac2c169d872d251a7440e
jewan100 <hawaii1468@gmail.com>
2026-01-26 15:34:36 +0900

    feat: Plan에서 불필요한 엔드포인트 제거

--- src/main/kotlin/com/epsilon/nagginggnome/domain/plan/controller/PlanController.kt
+++ src/main/kotlin/com/epsilon/nagginggnome/domain/plan/controller/PlanController.kt
@@ -3,15 +3,10 @@ package com.epsilon.nagginggnome.domain.plan.controller
 import com.epsilon.nagginggnome.domain.plan.dto.request.PlanCreateRequest
 import com.epsilon.nagginggnome.domain.plan.dto.request.PlanUpdateRequest
 import com.epsilon.nagginggnome.domain.plan.dto.response.PlanCreateResponse
-import com.epsilon.nagginggnome.domain.plan.dto.response.PlanDetailResponse
-import com.epsilon.nagginggnome.domain.plan.dto.response.PlanListItemResponse
 import com.epsilon.nagginggnome.domain.plan.dto.response.PlanUpdateResponse
 import com.epsilon.nagginggnome.domain.plan.service.PlanService
 import com.epsilon.nagginggnome.global.constant.code.CommonSuccessCode
 import com.epsilon.nagginggnome.global.dto.response.ApiResponse
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
-import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
@@ -25,55 +20,6 @@ import java.util.*
 class PlanController(
     private val planService: PlanService
 ) {
-
-    /**
-     * 플랜 리스트 조회 API
-     */
-    @GetMapping
-    fun getPlanList(
-        @AuthenticationPrincipal userId: UUID,
-        @PageableDefault(size = 20) pageable: Pageable
-    ): ResponseEntity<ApiResponse<Page<PlanListItemResponse>>> {
-        val res = planService.getMyPlans(userId, pageable)
-        return ResponseEntity.ok(ApiResponse.success(CommonSuccessCode.SUCCESS, res))
-    }
-
-    /**
-     * 플랜 상세 조회 API
-     */
-    @GetMapping("/{planId}")
-    fun getPlan(
-        @AuthenticationPrincipal userId: UUID,
-        @PathVariable planId: Long
-    ): ResponseEntity<ApiResponse<PlanDetailResponse>> {
-        val res = planService.getPlanDetail(userId, planId)
-        return ResponseEntity.ok(ApiResponse.success(CommonSuccessCode.SUCCESS, res))
-    }
-
-    /**
-     * 플랜 스냅샷 버전 목록 조회 API
-     */
-    @GetMapping("/{planId}/snapshots/versions")
-    fun getSnapshotVersions(
-        @AuthenticationPrincipal userId: UUID,
-        @PathVariable planId: Long
-    ): ResponseEntity<ApiResponse<List<Int>>> {
-        val res = planService.getSnapshotVersions(userId, planId)
-        return ResponseEntity.ok(ApiResponse.success(CommonSuccessCode.SUCCESS, res))
-    }
-
-    /**
-     * 플랜 특정 버전 상세 조회 API
-     */
-    @GetMapping("/{planId}/snapshots/{snapshotVersion}")
-    fun getPlanSnapshotVersion(
-        @AuthenticationPrincipal userId: UUID,
-        @PathVariable planId: Long,
-        @PathVariable snapshotVersion: Int
-    ): ResponseEntity<ApiResponse<PlanDetailResponse>> {
-        val res = planService.getPlanSnapshotVersionDetail(userId, planId, snapshotVersion)
-        return ResponseEntity.ok(ApiResponse.success(CommonSuccessCode.SUCCESS, res))
-    }
 
     /**
      * 플랜 생성 API
@@ -93,7 +39,7 @@ class PlanController(
     @PostMapping("/{planId}")
     fun updatePlan(
         @AuthenticationPrincipal userId: UUID,
-        @PathVariable planId: Long,
+        @PathVariable planId: UUID,
         @RequestBody req: PlanUpdateRequest
     ): ResponseEntity<ApiResponse<PlanUpdateResponse>> {
         val res = planService.updatePlan(userId, planId, req)
@@ -106,7 +52,7 @@ class PlanController(
     @DeleteMapping("/{planId}")
     fun deletePlan(
         @AuthenticationPrincipal userId: UUID,
-        @PathVariable planId: Long
+        @PathVariable planId: UUID
     ): ResponseEntity<ApiResponse<Nothing>> {
         planService.deletePlan(userId, planId)
         return ResponseEntity.ok(ApiResponse.success(CommonSuccessCode.SUCCESS))
