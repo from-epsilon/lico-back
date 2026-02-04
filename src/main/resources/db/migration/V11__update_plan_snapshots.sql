e067666db584e379312675eea1e4ec88cd55ae32
jewan100 <hawaii1468@gmail.com>
2026-02-04 12:05:47 +0900

    feat: Plan 상태 추가

--- src/main/resources/db/migration/V11__update_plan_snapshots.sql
+++ src/main/resources/db/migration/V11__update_plan_snapshots.sql
@@ -11,3 +11,10 @@ ALTER TABLE public.plan_snapshots
 ALTER TABLE public.plan_snapshots
     ADD CONSTRAINT ck_plan_snapshots_type
         CHECK (type IN ('CREATE', 'UPDATE', 'STATUS_CHANGE'));
+
+ALTER TABLE public.plans
+    DROP CONSTRAINT IF EXISTS ck_plans_status;
+
+ALTER TABLE public.plans
+    ADD CONSTRAINT ck_plans_status
+        CHECK (status IN ('ACTIVE', 'INACTIVE', 'DELETED', 'DONE'));
